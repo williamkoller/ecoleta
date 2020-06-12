b49c8877e9fa8d6b29a1888532a306c2cfd7b45b
William Koller <wkoller25@gmail.com>
2020-06-11 23:57:46 -0300

    feat: finish application

--- src/database/database.js
+++ src/database/database.js
@@ -1,2 +1,60 @@
 const database = require('sqlite3').verbose()
-const db = new database.Database('./src/database.database.db')
+
+const db = new database.Database('./src/database/database.db')
+
+// db.serialize(() => {
+//     db.run(`
+//         CREATE TABLE IF NOT EXISTS places (
+//             id INTEGER PRIMARY KEY AUTOINCREMENT,
+//             image TEXT,
+//             name TEXT,
+//             address TEXT,
+//             complement_number TEXT,
+//             state TEXT,
+//             city TEXT,
+//             items TEXT
+//             )
+//             `)
+//     const query = `
+//         INSERT INTO places (
+//             image,
+//             name,
+//             address,
+//             complement_number,
+//             state,
+//             city,
+//             items
+//             ) VALUES (?,?,?,?,?,?,?)
+//             `
+//     const values = [
+//         'https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+//         'Colectoria',
+//         'Guilherme Changretta, Jardim Jordão',
+//         '280',
+//         'Santa Catarina',
+//         'Rio do Sul',
+//         'Resíduos Eletrônicos, Lâmpadas',
+//     ]
+
+//     function afterInsertData(err) {
+//         if (err) {
+//             return console.error(err)
+//         }
+//         console.log('Cadastrado com sucesso!', this)
+//     }
+//     db.run(query, values, afterInsertData)
+//         // db.run(`DELETE FROM places`, (err) => {
+//         //         if (err) {
+//         //             return console.error(err)
+//         //         }
+//         //         console.log('Seu registro foi deletado')
+//         //     })
+//     db.all(`SELECT * FROM places`, function(err, rows) {
+//         if (err) {
+//             return console.error(err)
+//         }
+//         console.log('Aqui estão seus registros: ', rows)
+//     })
+// })
+
+module.exports = db
--- src/server.js
+++ src/server.js
@@ -1,29 +1,75 @@
 const express = require('express')
 const server = express()
+const db = require('./database/database')
 const port = 3000
 const desc = 'Server running on'
 server.use(express.static('public'))
-
+server.use(express.urlencoded({ extended: true }))
 
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
-  express: server,
-  noCache: true,
+    express: server,
+    noCache: true,
 })
 
-
 server.get('/', (req, res) => {
-  res.render('index.html')
+    res.render('index.html')
 })
 
 server.get('/create-point', (req, res) => {
-  res.render('create-point.html')
+    res.render('create-point.html')
+})
+
+server.post('/save-point', (req, res) => {
+    const query = `
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        complement_number,
+        state,
+        city,
+        items
+        ) VALUES (?,?,?,?,?,?,?);
+        `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.complement_number,
+        req.body.state,
+        req.body.city,
+        req.body.items,
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.error(err)
+            return res.send('Erro no cadastro')
+        }
+        console.log('Cadastrado com sucesso!', this)
+        return res.render('create-point.html', { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
 })
 
 server.get('/search', (req, res) => {
-  res.render('search-results.html')
+    const search = req.query.search
+
+    if (search == '') {
+        return res.render('search-results.html', { total: 0 })
+    }
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, (err, rows) => {
+        if (err) {
+            return console.error(err)
+        }
+        const total = rows.length
+        return res.render('search-results.html', { places: rows, total })
+    })
 })
 
 server.listen(port, () => {
-  console.log(`${desc}: ${port}`)
+    console.log(`${desc}: ${port}'`)
 })
